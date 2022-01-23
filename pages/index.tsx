b226e75a4a7049e6c2b14f2576d132c925f75415
Daniel Beckley <66218792+coachlivinglegend@users.noreply.github.com>
2022-01-23 17:20:06 +0100

    not in typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import Head from 'next/head';
+import { GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next'
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
 import Link from 'next/link';
 import Date from '../components/date';
 
-export async function getStaticProps() {
+export const getStaticProps:GetStaticProps = () => {
   const allPostsData = getSortedPostsData();
   return {
     props: {
